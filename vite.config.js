dda88463af7e9f87a161e5af8fd672a1aecd871a
zaid <zaidshabbir666@gmail.com>
2025-03-28 18:53:25 -0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://zayd100.github.io/React-proj/', // Change this to your actual repo name
+  base: '/React-Proj/', 
 });
